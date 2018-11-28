f69ced0173c944194e1a12eceb9c19117fe9bc49
Phellipehz <phellipeh@alu.ufc.br>
2018-11-28 02:26:51 -0300

    url aws

--- src/app/services/remote/remote.service.ts
+++ src/app/services/remote/remote.service.ts
@@ -11,8 +11,8 @@ import {Order } from 'src/app/classes/order';
 })
 export class RemoteService {
 
-  //host = 'http://ec2-3-16-164-126.us-east-2.compute.amazonaws.com:8082/';
-  host = "http://localhost:8082/";
+  host = 'http://ec2-3-16-164-126.us-east-2.compute.amazonaws.com:8082/';
+  //host = "http://localhost:8082/";
 
   constructor(private http: HttpClient, private tokenService: TokenPersistenceService) {}
 
